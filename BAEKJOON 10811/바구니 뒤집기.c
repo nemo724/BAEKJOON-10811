2b22b06b101254d918d01842cf66e7550a8b132c
nemo <nemo724@naver.com>
2024-09-19 13:12:29 +0900

    입력받은 구간을 역순 시키는 함수 추가

--- BAEKJOON 10811/바구니 뒤집기.c
+++ BAEKJOON 10811/바구니 뒤집기.c
@@ -9,6 +9,7 @@ int reverse_max = 0;
 void arr_len_and_try_change_input(arr_len, try_chage);
 void arr_reset(int[]);
 void reverse_interval(void);
+void try_reverse(int[],int,int,int);
 
 
 
@@ -23,6 +24,15 @@ void arr_reset(int arr[])
 void reverse_interval(void)
 {scanf_s("%d%d", &reverse_min, &reverse_max);}
 
+void try_reverse(int arr[],int element_count,int real_reverse_min_arr_index, int real_reverse_max_arr_index)
+{int two_point = element_count / 2;
+for (int j = 0; j < two_point; j++)
+{int temporary_serve = arr[real_reverse_max_arr_index-j];
+arr[real_reverse_max_arr_index - j] = arr[real_reverse_min_arr_index + j];
+arr[real_reverse_min_arr_index + j] = temporary_serve;
+}
+}
+
 
 
 
@@ -32,8 +42,10 @@ int main(void)
 int* arr = malloc(sizeof(int) * arr_len);
 arr_reset(arr);
 reverse_interval();
+
 int element_count = (reverse_max - reverse_min) + 1;
 int real_reverse_min_arr_index = reverse_min - 1;
 int real_reverse_max_arr_index = reverse_max - 1;
+
 
 }
